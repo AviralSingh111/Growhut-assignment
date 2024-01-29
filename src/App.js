99cddd948a6308940679a6df48c8de8b26385253
Aviral Singh <aviralsingh360@gmail.com>
2024-01-30 00:24:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
 import './App.css';
-
+import bg from './assets/group-1244833677.png'
+import ellipse from './assets/ellipse-331.png'
+import id from './assets/group-1244833678.png'
+import aca from './assets/group-1244833679.png'
+import ccp from './assets/group-1244833680.png'
+import mask from './assets/mask-group.png'
+import call from './assets/xnix-line-call.png'
+import line1 from './assets/line-160.png'
+import lineenvelope from './assets/xnix-line-envelope.png'
+import line2 from './assets/line-161.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div class="landing-page-3-t45">
+  <div class="auto-group-8ojs-bz5">
+    <img class="group-1244833677-WDs" src={bg} alt='abc'/>
+    <img class="ellipse-331-c21" src={ellipse} alt='abc'/>
+    <div class="understand-our-mission-YAZ">Understand Our Mission</div>
+    <div class="screenshot-314-1-a7F">
     </div>
+    <div class="ellipse-332-EBo">
+    </div>
+    <div class="ellipse-339-82H">
+    </div>
+    <div class="ellipse-341-RGH">
+    </div>
+    <div class="group-1244833685-8Ah">
+      <div class="frame-1597881810-dt9">
+        <div class="frame-1597881809-A7P">
+          <img class="group-1244833678-U85" src={id} alt='abc'/>
+          <p class="intuitive-dashboards-Nz9">Intuitive Dashboards</p>
+        </div>
+        <p class="user-friendly-interface-providing-seamless-navigation-and-effortlessly-accessible-data-insights-facilitating-a-streamlined-workflow-d2u">User-friendly interface providing seamless navigation and effortlessly accessible data insights facilitating a streamlined workflow.</p>
+      </div>
+      <div class="frame-1597881812-gms">
+        <div class="frame-1597881811-RjT">
+          <img class="group-1244833679-Mt1" src={aca} alt='abc'/>
+          <p class="accurate-risk-assessment-6qb">Accurate Risk Assessment</p>
+        </div>
+        <p class="analyzing-uncertainties-and-severity-of-regulations-and-circulars-to-safeguard-operations-and-enhance-proactive-decision-making-2UM">Analyzing uncertainties and severity of regulations and circulars to safeguard operations and enhance proactive decision-making.</p>
+      </div>
+      <div class="frame-1597881814-uo3">
+        <div class="frame-1597881813-4R3">
+          <img class="group-1244833680-CXF" src={ccp} alt='abc'/>
+          <p class="compliance-co-pilot-wUq">Compliance Co-Pilot</p>
+        </div>
+        <p class="navigating-regulatory-landscapes-with-precision-and-ease-your-trusted-partner-in-ensuring-adherence-to-industry-standards-stH">Navigating regulatory landscapes with precision and ease, your trusted partner in ensuring adherence to industry standards.</p>
+      </div>
+    </div>
+    <div class="group-1244833702-mCy">
+      <p class="secure-a-competitive-advantage-in-5UZ">Secure a competitive advantage in</p>
+      <img class="mask-group-CJH" src={mask} alt='abc'/>
+    </div>
+    <div class="ellipse-334-Y7F">
+    </div>
+    <div class="ellipse-338-STX">
+    </div>
+    <div class="group-1244833674-ZHF">
+      <div class="group-1244833672-62H">Company Email ID</div>
+      <div class="frame-1597881808-LxD">Join Waitlist</div>
+    </div>
+  </div>
+  <div class="auto-group-2xyd-2a9">
+    <div class="ellipse-345-jDf">
+    </div>
+    <div class="ellipse-346-R6V">
+    </div>
+    <div class="ellipse-347-KSm">
+    </div>
+    <div class="ellipse-348-E3w">
+    </div>
+  </div>
+  <div class="group-1244833681-M8Z">
+    <p class="growhut-rb7">Growhut</p>
+    <div class="frame-1597881802-BNV">
+      <div class="frame-1597881801-8Hj">
+        <div class="frame-1597881803-Geq">
+          <img class="xnix-line-call-Qm3" src={call} alt='abc'/>
+          <div class="contact-Xqf">Contact</div>
+        </div>
+        <img class="line-160-TzD" src={line1} alt='abc'/>
+        <p class="item-91-9876543210-1F3">+91 9876543210</p>
+      </div>
+      <div class="frame-1597881800-XUH">
+        <div class="frame-1597881804-UuK">
+          <img class="xnix-line-envelope-2A9" src={lineenvelope} alt='abc'/>
+          <div class="email-us-LwX">Email Us</div>
+        </div>
+        <img class="line-161-fyo" src={line2} alt='abc'/>
+        <p class="xyz123emailcom-bsT">xyz123@email.com</p>
+      </div>
+    </div>
+  </div>
+  <img class="vector-458-KHf" src="REPLACE_IMAGE:1:89" alt='abc'/>
+  <img class="vector-460-roP" src="REPLACE_IMAGE:1:90" alt='abc'/>
+  <div class="auto-group-dvux-CMT">
+    <div class="ellipse-336-hp1">
+    </div>
+    <div class="auto-group-9o5f-os3">
+      <div class="compliance-management-redefined-simple-smart-productive-Y3w">
+      Compliance Management Redefined
+      <br/>
+      Simple, Smart, Productive
+      </div>
+      <div class="ellipse-337-qYq">
+      </div>
+    </div>
+    <p class="seamless-workflows-crafted-by-ai-innovation-ZDw">Seamless workflows, Crafted by AI Innovation</p>
+    <div class="ellipse-335-rD3">
+    </div>
+  </div>
+  <div class="group-1244833682-NSH">Growhut</div>
+</div>
+</div>
   );
 }
 
